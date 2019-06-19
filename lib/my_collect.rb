76ae9f8441d127ec8454e4e80087a936a92375b6
Amin Amos <aminamosahs@gmail.com>
2019-06-19 01:59:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(arg)
   
   i = 0
   newarray = []
-  while i < arg.length
-    newarray << yield arg[i]
-    i += 1
-  end
+    while i < arg.length
+      newarray << yield arg[i]
+      i += 1
+    end
   newarray
 end
 
